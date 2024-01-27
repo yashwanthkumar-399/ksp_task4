63c96975fdb916876fa1228691127be885b656f2
yashwanthkumar-399 <127759617+yashwanthkumar-399@users.noreply.github.com>
2024-01-27 10:31:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var modulus = num1 % num2;
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Modulus:", modulus);
+if (num1 > num2) {
+    console.log("num1 is greater than num2");
+} else if (num1 < num2) {
+    console.log("num1 is less than num2");
+} else {
+    console.log("num1 is equal to num2");
+}
+
+if (num1 % 2 === 0) {
+    console.log("num1 is even");
+} else {
+    console.log("num1 is odd");
+}
+
+if (num2 > 0) {
+    console.log("num2 is positive");
+} else if (num2 < 0) {
+    console.log("num2 is negative");
+} else {
+    console.log("num2 is zero");
+}
+
